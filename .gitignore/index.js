5b81648ea74239fd75e8586b980d5481a208e16d
Washikarei <washikarei@gmail.com>
2018-05-27 18:53:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,15 +14,15 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if(message.content[0] === PREFIX) {
         if(message.content === '/planning') {
-            message.channel.send(
+            message.channel.send("Commande de l'utilisateur: " + message.author.username);
+            bot.channels.get("450336414891245569").send(
 `Comme toutes les semaines communiquer moi vos disponibilité pour la semaine qui arrive.
 Pour me donner vos dispo faite comme ceci, ça me donneras une meilleur visibilité svp.
 **[Jours] - [Heure a Heure] - [Jeu]**
-         
+                     
 La prise de créneau a une deadline qui est fixé au Dimanche à 21h.
 Un petit :white_check_mark:  seras mis sur votre message pour indiqué qu'il est bien sur le planning
 `);
-            bot.channels.get("450336414891245569").send("Commande de l'utilisateur " + message.author.username);
         }
     }
 });
